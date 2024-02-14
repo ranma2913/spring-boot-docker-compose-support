43b65c968a3d027bc8b03ca81dcab7845040953a
Joel Sticha (ranma2913) <4295880+ranma2913@users.noreply.github.com>
2024-02-14 01:14:25 -0600

    :sparkles: create project

---
+++ src/main/java/com/ranma2913/springboot/dockercomposesupportexample/SpringBootDockerComposeSupportExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ranma2913.springboot.dockercomposesupportexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposeSupportExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDockerComposeSupportExampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/ranma2913/springboot/dockercomposesupportexample/SpringBootDockerComposeSupportExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ranma2913.springboot.dockercomposesupportexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeSupportExampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
